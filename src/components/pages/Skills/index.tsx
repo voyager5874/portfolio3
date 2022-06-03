f2c820e6f48e3da6d8b5a40532a741756f2849c0
Aleksandr Savkin <voyager5874@gmail.com>
2022-06-03 23:31:09 +0300

    add responsiveness to TagCloud element

--- src/components/pages/Skills/index.tsx
+++ src/components/pages/Skills/index.tsx
@@ -1,19 +1,23 @@
 import "./index.scss";
-import {useEffect} from "react";
+import {useEffect, useRef, useState} from "react";
 // @ts-ignore
 import TagCloud from "TagCloud"
 // const TagCloud = require('TagCloud');
+import Loader from 'react-loaders';
+import {PageCaption} from "../../pageCaption";
+
 
 type TagCloudOptionsType = Partial<{
-    keep: boolean
+    keep: boolean //keep rolling after mouse leave the area
     maxSpeed: 'slow' | 'normal' | 'fast'
     radius: number
     initSpeed: 'slow' | 'normal' | 'fast'
     direction: number
-    // containerClass: string
+    containerClass: string // why? you have to pass container class to constructor anyway
     itemClass: string
     useContainerInlineStyles: boolean
     useItemInlineStyles: boolean
+
 }>
 
 const TagCloudContainerClassName = '.tagcloud';
@@ -23,22 +27,46 @@ const myTags = [
 ];
 
 export const Skills = () => {
+    const cloudContainer = useRef<HTMLDivElement>(null)
+    const [cloudRadius, setCloudRadius] = useState(150)
 
     const options: TagCloudOptionsType = {
-        radius: 250,
+        radius: cloudRadius,
         maxSpeed: 'fast',
         initSpeed: 'fast',
     }
+
     useEffect(() => {
         const tagInstance = TagCloud(TagCloudContainerClassName, myTags, options)
+        // console.log(tagInstance)
         return () => {
             tagInstance.destroy()
         }
-    }, [])
-    return (
-        <div className="cloud-wrapper">
-            <div className="tagcloud"/>
-        </div>
+    }, [cloudRadius])
 
+    useEffect(() => {
+        if (cloudContainer.current) setCloudRadius(cloudContainer.current.clientWidth / 2)
+
+        function handleResize() {
+            if (!cloudContainer.current) return
+            setCloudRadius((cloudContainer.current.clientWidth) / 2)
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+
+    return (
+        <>
+            <div className="skills-page">
+                <PageCaption delay={20} duration={3500} text={"Tools I'm using"}/>
+                <div ref={cloudContainer} className="tagcloud"/>
+            </div>
+
+            <Loader type={"pacman"} active/>
+        </>
     )
 };
